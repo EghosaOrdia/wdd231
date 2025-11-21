1058fb744ca5e5fbaf33eeeb0c2cefa78870f08b
Eghosa Ordia <eghodia1004@yahoo.com>
2025-11-21 16:15:59 +0100

    feat: experimenting with dialog

--- scripts/main.js
+++ scripts/main.js
@@ -43,3 +43,14 @@ tabBtns.forEach((btn) => {
     sumCredits();
   });
 });
+
+const modal = document.getElementById("modal");
+const openModal = document.querySelector(".open-modal");
+const closeModal = document.querySelector(".close-modal");
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", function () {
+  modal.close();
+});
