870ad75d11946b6a3d51df540d23bc52f5f79cd2
Eghosa Ordia <eghodia1004@yahoo.com>
2025-12-06 20:30:45 +0100

    feat: completed mobile view for hero, search section, steps section & started whychoose section

---
+++ final/scripts/main.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("nav");
+// const lastModified = document.getElementById("lastModified");
+
+menuToggle.addEventListener("click", function () {
+  menuToggle.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+// lastModified.textContent = document.lastModified;
